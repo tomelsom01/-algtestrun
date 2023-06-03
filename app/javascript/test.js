0ac8da362c4cd782c43899c969e9086836424345
tomelsom <te28@hotmail.com>
2023-06-04 00:06:45 +0100

    trying fetch

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -9,3 +9,9 @@ console.log("hello from app/javascript/test.js! we are finally working");
 
 //Display!!
 //put on the screen in the correct order
+
+fetch("https://gist.githubusercontent.com/alexandremeunier/49533eebe2ec93b14d32b2333272f9f8/raw/924d89e2236ca6fa2ade7481c91bfbf858c49531/movies.json")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
