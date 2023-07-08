6d6092a01f990aacb00cded1b92c597fe3f6ebaa
tomelsom <te28@hotmail.com>
2023-07-08 21:13:14 +0100

    adding a search option

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,3 +1,5 @@
+const { preventOverflow } = require("@popperjs/core");
+
 console.log("hello from app/javascript/test.js! we are finally working");
 
 // Get!!
@@ -12,9 +14,27 @@ console.log("hello from app/javascript/test.js! we are finally working");
 
 const list = document.querySelector("#list-inline");
 
-
-fetch("https://www.omdbapi.com/?s=fast%20and%20furious&apikey=adf1f2d7")
+const fetchMovies = () => {
+  fetch("https://www.omdbapi.com/?s=fast%20and%20furious&apikey=adf1f2d7")
   .then(response => response.json())
   .then((data) => {
     console.log(data);
+    data.Search.forEach(result => {
+      console.log(result);
+      const movie = `<li class="list-inline-item">
+        <img src="${result.Poster}">
+        <p>${result.Title}</p>
+      </li>
+      `;
+      list.insertAdjacentHTML("beforeend", movie);
+    });
   });
+};
+
+const form = document.getElementById("search-form");
+
+form.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  const input = event.currentTarget.querySelector("#search-input");
+  console.log(input);
+});
