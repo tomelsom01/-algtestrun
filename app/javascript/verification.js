4ade37ccee6121b2468b76de629e566668aef586
tomelsom <te28@hotmail.com>
2023-07-16 00:06:47 +0100

    added search and sign in feature development

--- app/javascript/verification.js
+++ app/javascript/verification.js
@@ -6,10 +6,14 @@ const signUp = (event) => {
   console.log(emailValue.value);
   console.log(passwordValue.value);
   fetch("https://reqres.in/api/register", {
-    method: "Post",
-    headers: { "Content-Type": "application/json" }
-  })
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: emailValue.value, password: passwordValue.value })
+    })
+    .then(response => response.json())
+    .then((data) => console.log(data))
 };
 
 const form = document.querySelector("#form");
 form.addEventListener("submit", signUp);
+
